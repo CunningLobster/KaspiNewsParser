d0b9379eb9ec151eab3f5b6da23a5882d0231a40
cherepansky.yan <cherepansky.yan@yandex.ru>
2023-12-20 00:13:15 +0600

    rename post getter contract

---
+++ NewsParser.Core/ServiceContracts/INewsPostGetterService.cs
@@ -0,0 +1,23 @@
+using NewsParser.Core.DTO;
+using NewsParser.Core.Entities;
+
+namespace NewsParser.Core.ServiceContracts
+{
+    public interface INewsPostGetterService
+    {
+        /// <summary>
+        /// Получить новостной пост из HTML элемента
+        /// </summary>
+        /// <param name="htmlElement"></param>
+        /// <returns></returns>
+        public NewsPost? GetNewsPostFromHtml(HtmlDto htmlElement);
+        /// <summary>
+        /// Получить список новостных постов в промежутке между датами
+        /// </summary>
+        /// <param name="newsPosts">Список новостных постов</param>
+        /// <param name="from">Дата с</param>
+        /// <param name="to">Дата по</param>
+        /// <returns>Отфильтрованный список постов</returns>
+        public List<NewsPost> GetNewsPostsByDates(List<NewsPost> newsPosts, DateTime? from, DateTime? to);
+    }
+}
